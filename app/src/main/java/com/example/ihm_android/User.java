308fe71731d5d0aae15aaf1d78bd3b34b0452f1f
wukejia1 <kejia.wu@etu.unice.fr>
2019-05-06 04:19:25 +0800

    changement of class user

--- app/src/main/java/com/example/ihm_android/User.java
+++ app/src/main/java/com/example/ihm_android/User.java
@@ -7,15 +7,25 @@ import java.util.Date;
 public class User {
     private String username;
     private String password;
-    private String nom;
-    private String prenom;
+    private String lastName;
+    private String firstName;
+    private String smallDescription;
+    private String longDescription;
 
     User(String username,String password){
         this.username = username;
         this.password = password;
     }
 
-    String getNom(){ return this.nom;}
-    String getPrenom(){return this.prenom;}
-    String getUsername(){return this.username;}
+    public void setLastName(String lastName){this.lastName = lastName;}
+    public void setFirstName(String firstName){this.firstName = firstName;}
+    public void setSmallDescription(String smallDescription){this.smallDescription = smallDescription;}
+    public void setLongDescription(String longDescription){this.longDescription = longDescription;}
+
+    public String getUsername() {return username;}
+    public String getPassword(){return password;}
+    public String getLastName(){return lastName;}
+    public String getFirstName(){return firstName;}
+    public String getSmallDescription(){return smallDescription;}
+    public String getLongDescription(){return longDescription;}
 }
