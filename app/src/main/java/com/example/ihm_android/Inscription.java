903e5e27875fea4ff447dbf42d97102b785bc339
wukejia1 <kejia.wu@etu.unice.fr>
2019-05-06 05:21:04 +0800

    class inscription

--- app/src/main/java/com/example/ihm_android/Inscription.java
+++ app/src/main/java/com/example/ihm_android/Inscription.java
@@ -1,4 +1,32 @@
 package com.example.ihm_android;
 
-public class Inscription {
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import java.text.ParseException;
+
+public class Inscription extends AppCompatActivity {
+    private TextView Txt_password_con;
+    private TextView Txt_password;
+    private TextView Txt_username;
+
+    public Inscription() throws ParseException {
+    }
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inscription);
+//        view =this.getLayoutInflater().inflate(R.layout.ajouteraliments,null);
+//        setContentView(view);
+
+        Txt_username = (TextView) findViewById(R.id.Txt_username);
+        Txt_password = (TextView) findViewById(R.id.Txt_password);
+        Txt_password_con= (TextView) findViewById(R.id.Txt_password_confirmed);
+    }
+
 }
