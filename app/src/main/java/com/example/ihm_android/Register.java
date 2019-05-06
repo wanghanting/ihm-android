ef103ef73854546f7ce7b5dad275460b5920e92f
DCRUZ-Celia <celia.d'cruz@etu.univ-cotedazur.fr>
2019-05-06 21:49:02 +0200

    Implémentation des errors dans Register

--- app/src/main/java/com/example/ihm_android/Register.java
+++ app/src/main/java/com/example/ihm_android/Register.java
@@ -11,7 +11,7 @@ import android.widget.TextView;
 public class Register extends AppCompatActivity implements View.OnClickListener{
 
     Button bRegister;
-    EditText etUsername, etPassword;
+    EditText etUsername, etPassword, etConfirmationPassword;
     TextView tvLoginLink;
     Data data;
 
@@ -25,6 +25,7 @@ public class Register extends AppCompatActivity implements View.OnClickListener{
         bRegister = (Button) findViewById(R.id.bRegister);
         etUsername = (EditText) findViewById(R.id.etUsername);
         etPassword = (EditText) findViewById(R.id.etPassword);
+        etConfirmationPassword = (EditText) findViewById(R.id.etConfirmationPassword);
         tvLoginLink = (TextView) findViewById(R.id.tvLoginLink);
 
         bRegister.setOnClickListener(this);
@@ -35,11 +36,25 @@ public class Register extends AppCompatActivity implements View.OnClickListener{
     public void onClick(View v){
         switch(v.getId()){
             case R.id.bRegister:
-                String username = etUsername.getText().toString();
-                String password = etPassword.getText().toString();
-                User user = new User(username, password);
-                data.user = user;
-                startActivity(new Intent(this, Profile.class));
+                if (etUsername.getText().toString().length() == 0){
+                    etUsername.setError("Entrer un nom d'utilisateur");
+                }
+                if (etPassword.getText().toString().length() == 0){
+                    etPassword.setError("Entrer un mot de passe");
+                }
+                if (etConfirmationPassword.getText().toString().length() == 0){
+                    etConfirmationPassword.setError("Entrer un mot de passe");
+                }
+                else if (!etConfirmationPassword.getText().toString().equals(etPassword.getText().toString())){
+                    etConfirmationPassword.setError("Mot de passe incorrect");
+                }
+                else{
+                    String username = etUsername.getText().toString();
+                    String password = etPassword.getText().toString();
+                    User user = new User(username, password);
+                    data.user = user;
+                    startActivity(new Intent(this, Profile.class));
+                }
                 break;
             case R.id.tvLoginLink:
                 startActivity(new Intent(this, Login.class));
